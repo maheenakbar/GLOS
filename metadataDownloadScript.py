9dae5bd496db248f0b87401ac36bc6089771a5d9
Alex V. Kahan <alkahan@umich.edu>
2019-04-11 14:23:12 -0400

    Update
    Added dynamic field mapping for indexing and added Quality Score and Grade functions

--- metadataDownloadScript.py
+++ metadataDownloadScript.py
@@ -21,37 +21,110 @@ def sanitize(value):
 def parse_metadata(metadata_df):
     for index, row in metadata_df.iterrows():
         yield {'_id' : index,
-        'schema' : sanitize(row[0]),
-        'uuid' : sanitize(row[1]), 
-        'id' : sanitize(row[2]), 
-        'title' : sanitize(row[3]), 
-        'abstract' : sanitize(row[4]), 
-        'keyword' : sanitize(row[5]),
-        'link' : sanitize(row[6]),
-        'responsibleParty' : sanitize(row[7]),
-        'metadatacreationdate' : sanitize(row[8]),
-        'geoBox' : sanitize(row[9]),
-        'image' : sanitize(row[10]),
-        'LegalConstraints' : sanitize(row[11]),
-        'temporalExtent' : sanitize(row[12]),
-        'parentId' : sanitize(row[13]),
-        'datasetcreationdate' : sanitize(row[14]),
-        'Constraints' : sanitize(row[15]),
-        'SecurityConstraints' : sanitize(row[16])
+        'schema' : sanitize(row['schema']),
+        'uuid' : sanitize(row['uuid']), 
+        'id' : sanitize(row['id']), 
+        'title' : sanitize(row['title']), 
+        'abstract' : sanitize(row['abstract']), 
+        'keyword' : sanitize(row['keyword']),
+        'link' : sanitize(row['link']),
+        'responsibleParty' : sanitize(row['responsibleParty']),
+        'metadatacreationdate' : sanitize(row['metadatacreationdate']),
+        'geoBox' : sanitize(row['geoBox']),
+        'image' : sanitize(row['image']),
+        'LegalConstraints' : sanitize(row['LegalConstraints']),
+        'temporalExtent' : sanitize(row['temporalExtent']),
+        'parentId' : sanitize(row['parentId']),
+        'datasetcreationdate' : sanitize(row['datasetcreationdate']),
+        'Constraints' : sanitize(row['Constraints']),
+        'SecurityConstraints' : sanitize(row['SecurityConstraints']),
+        'Quality Score' : sanitize(row['Quality Score']),
+        'Grade' : sanitize(row['Grade'])
               }
+def quality_score(df):
+    for row in df.iterrows():
+        rows = row[1]
+        qs = rows.count()
+        try:
+            kw = len(rows[5].split(','))
+
+        except:
+            kw = 0
+        
+        if kw < 2:
+            pass
+        elif kw >= 2 and kw < 4:
+            qs+=1
+        elif kw >= 4:
+            qs+=2  
+        try:
+            abst = len(rows[4].split(' '))
+    
+        except:
+            abst = 0
+    
+        if abst == 0:
+            pass
+        elif abst > 0 and abst < 100:
+            qs+=1
+        elif abst >= 100 and abst < 200:
+            qs += 2
+        elif abst > 200 and abst < 300:
+            qs += 3
+        elif abst >= 300:
+            qs += 4
+    
+        try:
+            link = len(rows[8].split())
+        except:
+            link = 0
+        if link < 2:
+            pass
+        elif link >=2 and link < 4:
+            qs += 1
+        elif link >=4:
+            qs += 2
+            
+        try:
+            date = rows.metadatacreationdate
+            year = int(date[3])
+        
+        except:
+            year = 0
+        qs += year
+
+        df.at[row[0], 'Quality Score'] = qs
+    
+    return (df)
+
+def grade(df):
+    for row in df.iterrows():
+        rows = row[1]
+        qs = rows[17]
+    
+        if qs < 13:
+            grade = 'F'
+        elif qs >= 13 and qs < 18:
+            grade = 'D'
+        elif qs >= 18 and qs < 23:
+            grade = 'C'
+        elif qs >= 23 and qs < 28:
+            grade = 'B'
+        elif qs >= 28:
+            grade = 'A'
+        df.at[row[0], 'Grade'] = grade
+    return (df)
 
 url = 'http://data.glos.us/metadata/srv/eng/csv.search?'
 r = requests.get(url, allow_redirects=True)
 open('metadata.txt', 'wb').write(r.content)
 
-# colnames = ["schema","uuid","id","title","abstract","keyword","link","responsibleParty","metadatacreationdate","geoBox","image","LegalConstraints","temporalExtent","parentId","datasetcreationdate","Constraints","SecurityConstraints"]
 with open('metadata.txt',errors='ignore') as f:
     colnames = f.readline().strip().replace('"','').split(",")
 
     contents = f.read() # type bytes
     df = pd.read_csv(StringIO(contents), sep='","',skiprows=[0],index_col=False,names=colnames, engine='python')
     df['schema'] = df['schema'].str.replace('"', ' ', regex=True)
-    #df['abstract'] = df['abstract'].str.replace('<\s*[^>]*>', ' ', regex=True)
     df['abstract'] = df['abstract'].str.replace('<p>', ' ', regex=True)
     df['abstract'] = df['abstract'].str.replace('</p>', ' ', regex=True)
     df['abstract'] = df['abstract'].str.replace('<h4>', ' ', regex=True)
@@ -64,9 +137,14 @@ with open('metadata.txt',errors='ignore') as f:
     df['geoBox'] = df['geoBox'].str.replace('###', ' ', regex=True)
     df['SecurityConstraints'] = df['SecurityConstraints'].str.replace('".', "", regex=True)
 
+    df = quality_score(df)
+    df = grade(df)
+
 es_conn = Elasticsearch(host_url, ca_certs = certifi.where())
 
 bulk(es_conn, parse_metadata(df), index = 'metadata', doc_type = 'record')
 
 export_csv = df.to_csv(r'clean_metadata2.csv', index = None, header=True)
+
+print ('Indexing Successful')
 
